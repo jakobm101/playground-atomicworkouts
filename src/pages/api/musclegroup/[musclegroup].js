21d050e93d8c6a1c33193d0678c1d84b3e9fb0d1
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-15 00:17:42 +0200

    query for exercises for musclegroups functional

--- src/pages/api/musclegroup/[musclegroup].js
+++ src/pages/api/musclegroup/[musclegroup].js
@@ -3,16 +3,19 @@ import Exercise from "../../../../db/Schema/Exercise";
 import libMusclegroups from "../../../../lib/musclegroups";
 
 export default async function handler(req, res) {
-  if (
-    !libMusclegroups.find(
-      (muscle) => muscle.toLowerCase() === req.query.musclegroup
-    )
-  )
+  let query = req.query.musclegroup;
+  // Capitalize
+  query = query[0].toUpperCase() + query.slice(1);
+
+  if (!libMusclegroups.find((muscle) => muscle === query))
     return res.status(500).json({ error: "not found" });
 
   try {
     await dbConnect();
-    const exercises = await Exercise.find();
+    const exercises = await Exercise.find({
+      muscleGroups: query,
+    });
+
     res.status(200).json(exercises);
   } catch (error) {
     res.status(500).json({ error: error.message });
