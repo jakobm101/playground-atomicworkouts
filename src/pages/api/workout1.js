2106a22a11351589f327f312af493208ffd81ff2
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-14 22:43:53 +0200

    cleanup

--- src/pages/api/workout1.js
+++ src/pages/api/workout1.js
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import dbConnect from "../../../db/connect";
 import Exercise from "../../../db/Schema/Exercise";
 import Workout from "../../../db/Schema/Workout";
